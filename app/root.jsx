f6af5cd76f6346e7a5023072c380f7dcba3f7a2c
Cuong Phan <codyreactista@gmail.com>
2022-11-24 14:21:12 +0700

    created project routes

--- app/root.jsx
+++ app/root.jsx
@@ -1,6 +1,3 @@
-import MainNavigation from "./components/MainNavigation";
-import styles from "./styles/main.css";
-
 const {
   Links,
   LiveReload,
@@ -8,8 +5,6 @@ const {
   Outlet,
   Scripts,
   ScrollRestoration,
-  Link,
-  useCatch,
 } = require("@remix-run/react");
 export const meta = () => ({
   charset: "utf-8",
@@ -25,9 +20,6 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <header>
-          <MainNavigation />
-        </header>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
@@ -35,63 +27,4 @@ export default function App() {
       </body>
     </html>
   );
-}
-
-export function CatchBoundary() {
-  const caughtResponse = useCatch();
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-        <title>{caughtResponse.statusText}</title>
-      </head>
-      <body>
-        <header>
-          <MainNavigation />
-        </header>
-        <main className="error">
-          <h1>{caughtResponse.statusText}</h1>
-          <p>{caughtResponse.data?.message || "Something went wrong!"}</p>
-          <p>
-            Back to <Link to="/">safety</Link>!
-          </p>
-        </main>
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
-}
-
-export function ErrorBoundary({ error }) {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-        <title>An error occurred!</title>
-      </head>
-      <body>
-        <header>
-          <MainNavigation />
-        </header>
-        <main className="error">
-          <h1>An error occurred!</h1>
-          <p>{error.message}</p>
-          <p>
-            Back to <Link to="/">safety</Link>!
-          </p>
-        </main>
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
-}
-
-export function links() {
-  return [{ rel: "stylesheet", href: styles }];
 }
