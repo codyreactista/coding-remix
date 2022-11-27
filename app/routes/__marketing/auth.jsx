5e8cf6d37d55f5dfbade30c0c7584be0ea94c6d9
Cuong Phan <codyreactista@gmail.com>
2022-11-27 16:00:40 +0700

    added user signup

--- app/routes/__marketing/auth.jsx
+++ app/routes/__marketing/auth.jsx
@@ -1,4 +1,7 @@
+import { redirect } from "@remix-run/node";
 import AuthForm from "~/components/auth/AuthForm";
+import { signup } from "~/data/auth.server";
+import { validateCredentials } from "~/data/validation.server";
 import authStyles from "~/styles/auth.css";
 
 export default function AuthPage() {
@@ -12,12 +15,23 @@ export async function action({ request }) {
   const formData = await request.formData();
   const credentials = Object.fromEntries(formData);
 
-  // validate user input
+  try {
+    validateCredentials(credentials);
+  } catch (error) {
+    return error;
+  }
 
-  if (authMode === "login") {
-    // login logic
-  } else {
-    // signup logic (create user)
+  try {
+    if (authMode === "login") {
+      // login logic
+    } else {
+      await signup(credentials);
+      return redirect("/expenses");
+    }
+  } catch (error) {
+    if (error.status === 422) {
+      return { credentials: error.message };
+    }
   }
 }
 
