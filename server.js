dacf9447f818fd72858e7b684cb592d5a6531c8b
Cuong Phan <codyreactista@gmail.com>
2022-11-29 11:25:25 +0700

    added vercel deployment

---
+++ server.js
@@ -0,0 +1,4 @@
+import * as build from "@remix-run/dev/server-build";
+import { createRequestHandler } from "@remix-run/node";
+
+export default createRequestHandler({ build, mode: process.env.NODE_ENV });
